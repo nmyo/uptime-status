bd45a226b0c8f2e6e8de20abc6144da3f8ad5692
fangxi <52951423+nmyo@users.noreply.github.com>
2024-02-24 15:24:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'ZGQ Inc. の 站点监测',
+  SiteName: 'Fangxi の 站点监测',
 
   // UptimeRobot API Keys
   // 支持Monitor-Specific API Keys和Read-Only API Key，推荐使用Read-Only API Key，可填写多个
   ApiKeys: [
-    'ur1884169-f2ddd4677953551305552673',
+    'ur1228130-26475f1a5eea49a1316db46f',
   ],
 
   // 日志天数，建议90
@@ -14,20 +14,4 @@ window.Config = {
 
   // 是否显示检测站点的链接
   ShowLink: true,
-
-  // 导航栏菜单
-  Navi: [
-    {
-      text: '个人主页',
-      url: 'https://domain.zgqinc.gq/'
-    },
-    {
-      text: '项目地址',
-      url: 'https://github.com/ZGQ-inc/uptime-status'
-    },
-    {
-      text: '原项目地址',
-      url: 'https://github.com/imsyy/status'
-    },
-  ],
 };
